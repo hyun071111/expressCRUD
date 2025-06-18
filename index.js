ebfb7a4c64a67f5a64c697c204c36fc92f72cf24
hyun071111 <jisaki07@gmail.com>
2025-06-18 10:13:59 +0900

    fix: Fixed date related errors in details

--- index.js
+++ index.js
@@ -35,14 +35,21 @@ const WritingSchema = new Schema({
 
 const Writing = mongoose.model('Writing', WritingSchema);
 
+// 날짜를 문자열로 변환하는 함수
+function formatDate(date) {
+    if (!date) return '';
+    const d = new Date(date);
+    return `${d.getFullYear()}년 ${d.getMonth() + 1}월 ${d.getDate()}일`;
+}
+
 // middleware
 // main page GET
 app.get('/', async (req, res) => {
-    // const fileData = fs.readFileSync(filePath);
-    // const writings = JSON.parse(fileData);
-
     let writings = await Writing.find({});
-
+    writings = writings.map(writing => ({
+        ...writing.toObject(),
+        date: formatDate(writing.date)
+    }));
     res.render('main', {list : writings });
 });
 
@@ -77,7 +84,15 @@ app.get('/detail/:id', async (req, res) => {
 
     try {
         const detail = await Writing.findOne({ _id: id });
-        res.render('detail', { detail });  // key와 변수명이 같으면 생략 가능
+        if (detail) {
+            const formattedDetail = {
+                ...detail.toObject(),
+                date: formatDate(detail.date)
+            };
+            res.render('detail', { detail: formattedDetail });
+        } else {
+            res.status(404).send('게시글을 찾을 수 없습니다.');
+        }
     } catch (err) {
         console.error(err);
         res.status(500).send('Internal Server Error');
